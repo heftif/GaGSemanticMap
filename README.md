2f4d2246a19cc70d2552091a708cfa44e70e1144
heftif <100215387+heftif@users.noreply.github.com>
2023-11-23 14:46:54 +0100

    Change File Structure

--- README.md
+++
@@ -1,2 +0,0 @@
-# GaGSemanticMap
- .NET 8 Blazor Application with Azure OpenAI and Semantic Search of Data Points
--- Services/ISemanticSearchService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Vector = Pgvector.Vector;
-
-namespace GaGSemanticMap.Services;
-
-public interface ISemanticSearchService
-{
-    Task<string> GetOpenAIResponseAsync(string userInput);
-	Task GetEventsBySemanticRelevanceAsync(string userInput);
-}
